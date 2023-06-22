a9b517af06bfb22b093ac2d1b7821cfc17160c6e
rohansx <sharmarohan615@gmail.com>
2023-06-23 00:33:33 +0530

    resolve

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,132 +1,123 @@
-const express = require("express")
-const { body, validationResult } = require("express-validator")
-const JWT = require("jsonwebtoken")
-const router = express.Router()
-const bcrypt = require("bcryptjs")
-const USER = []
-const { writeUsers, readUsers } = require("../utils/utils")
-const { SECRET } = require("../config")
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { writeUsers, readUsers } = require("../utils/utils");
+const { SECRET } = require("../config");
 
+const router = express.Router();
+
+// Endpoint for user registration
 router.post(
-	"/register",
-	body("name")
-		.custom((name) => {
-			if (typeof name === "string" && name.length >= 3) {
-				return true
-			}
-			return false
-		})
-		.withMessage("Name should be of minimum 3 characters."),
-	body("email")
-		.custom((email) => {
-			if (typeof email === "string") {
-				return true
-			}
-			return false
-		})
-		.withMessage("Please enter valid Email")
-		.isEmail()
-		.withMessage("Enter email in proper format. E.g example@xyz.com"),
-	body("password")
-		.custom((password) => {
-			if (typeof password === "string" && password.length >= 8) {
-				return true
-			}
-			return false
-		})
-		.withMessage("Password should be of minimum 8 characters."),
-	async (req, res) => {
-		const { name, email, password } = req.body
+  "/register",
+  // Request body validation using express-validator
+  [
+    body("name")
+      .isString()
+      .isLength({ min: 3 })
+      .withMessage("Name should be at least 4 characters long."),
+    body("email")
+      .isString()
+      .isEmail()
+      .withMessage("Please enter a valid email."),
+    body("password")
+      .isString()
+      .isLength({ min: 8 })
+      .withMessage("Password should be at least 8 characters long."),
+  ],
+  async (req, res) => {
+    const { name, email, password } = req.body;
 
-		const errors = validationResult(req)
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        message: "User registration failed.",
+        error: errors.array(),
+        data: {},
+      });
+    }
 
-		if (!errors.isEmpty()) {
-			return res.status(400).json({
-				message: "User registration failed.",
-				error: errors.array(),
-				data: {},
-			})
-		}
-		// try{
-		// const salt = await bcrypt.genSalt(10)
+    try {
+      // Hash the password using bcrypt
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-		let hashedPassword = await bcrypt.hashSync(password, 10)
+      // Create a new user object
+      const newUser = {
+        name,
+        email,
+        password: hashedPassword,
+      };
 
-		console.log(hashedPassword)
-		console.log(password)
-		USER.push({
-			name: name,
-			email: email,
-			password: hashedPassword,
-		})
-		writeUsers(USER)
+      // Add the user to the USER array
+      const users = await readUsers();
+      users.push(newUser);
+      writeUsers(users);
 
-		return res.status(201).json({
-			message: "User registration successful.",
-			error: null,
-			data: {
-				newUser,
-				access_token: token,
-				user: newUser.name,
-			},
-		})
+      // Generate a JWT access token
+      const token = jwt.sign({ email }, SECRET);
 
-		// catch (error) {
-		// 	console.error("Error: ", error.message)
-		// 	res.status(500).send("Internal server error occured.")
-		// }
-	}
-)
+      return res.status(201).json({
+        message: "User registration successful.",
+        error: null,
+        data: {
+          newUser,
+          access_token: token,
+          user: newUser.name,
+        },
+      });
+    } catch (error) {
+      console.error("Error:", error.message);
+      return res.status(500).send("Internal server error occurred.");
+    }
+  }
+);
 
+// Endpoint for user login
 router.post("/login", async (req, res) => {
-	const { email, password } = req.body
+  const { email, password } = req.body;
 
-	console.log("---user info ---", email, password)
-	let users = await readUsers()
-	console.log(users)
+  try {
+    // Read users from storage
+    const users = await readUsers();
 
-	USER.push(...users)
-	if (USER.length <= 0) {
-		return res.status(400).json({
-			message: "User login failed.",
-			error: "User does not exists.",
-			data: {},
-		})
-	}
+    // Find the user with the provided email
+    const user = users.find((user) => user.email === email);
 
-	const userIndex = USER.findIndex((user) => user.email === email)
+    if (!user) {
+      // User not found
+      return res.status(404).json({
+        message: "User login failed.",
+        error: "User not found.",
+        data: {},
+      });
+    }
 
-	if (userIndex === -1) {
-		return res.status(404).json({
-			message: "User login failed.",
-			error: "User not found.",
-			data: {},
-		})
-	}
-	var match = bcrypt.compareSync(password, USER[userIndex].password)
+    // Compare passwords using bcrypt
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      // Invalid password
+      return res.status(404).json({
+        message: "User login failed.",
+        error: "Invalid password.",
+        data: {},
+      });
+    }
 
-	console.log(password)
-	console.log(password)
+    // Generate a JWT access token
+    const token = jwt.sign({ email }, SECRET);
 
-	if (match === false) {
-		return res.status(404).json({
-			message: "User login failed.",
-			error: "Invalid password.",
-			data: {},
-		})
-	}
-	// create access tokens
-	// response to clientjwt npm
+    return res.status(200).json({
+      message: "User login successful.",
+      error: null,
+      data: {
+        access_token: token,
+      },
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+    return res.status(500).send("Internal server error occurred.");
+  }
+});
 
-	const token = JWT.sign({ email }, SECRET)
-
-	return res.status(200).json({
-		message: "User login successful.",
-		error: null,
-		data: {
-			access_token: token,
-		},
-	})
-})
-
-module.exports = router
+module.exports = router;
